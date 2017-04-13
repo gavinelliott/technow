88f80552a29ec8c241bf70ac6a41566e336113ca
gavinelliott <gavin@gavinelliott.co.uk>
2017-04-13 11:48:29 +0100

    Start afresh

--- index.js
+++
@@ -1,9 +0,0 @@
-// Config App
-var express     = require('express');
-var app         = express();
-
-// Setup Router
-app.use(express.static(__dirname + '/public'));
-
-// Server
-app.listen(process.env.PORT || 3002);
