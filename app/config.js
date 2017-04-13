861cd3f52be7daaf66893fe02c1134cd66811180
gavinelliott <gavin@gavinelliott.co.uk>
2017-04-13 17:07:53 +0100

    New build

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Technow',
 
   // Default port that prototype runs on
   port: '3000',
